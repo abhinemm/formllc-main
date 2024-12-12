ef6d601418dfbbcd3318d0e5fff8c96411c425d3
Christin <christink388@gmail.com>
2024-12-13 01:37:49 +0530

    initial commit

---
+++ components/Footer/Foooter.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Footer.module.scss";
+
+const Foooter = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <p>© 2024 Business Globalizer. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Foooter;

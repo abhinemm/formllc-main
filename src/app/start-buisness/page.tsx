447fda8dd6f7263215363c5c258de2a8880ed8e1
Christin <christink388@gmail.com>
2024-12-23 10:57:28 +0530

    fix: changes

--- src/app/start-buisness/page.tsx
+++ src/app/start-buisness/page.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import StartBusinessTabs from "../../../components/BuisnessRegister/StartBuisness";
-import styles from './start.module.scss'
-
 const page = () => {
   return (
     <div>

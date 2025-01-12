6cc67f3faa0bb61bb1f7e8ec2db96eadeab7c729
rj001-10 <68068858+rj001-10@users.noreply.github.com>
2025-01-12 08:48:51 +0000

    stripe

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,5 +1,201 @@
-export async function POST(req: Request) {
-    const body = await req.body;
-    console.log("req------------->",body);
+import Company from "@/models/company";
+import Payments from "@/models/payments.model";
+import { PlansEnum } from "@/utils/constants";
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const BASIC_PLAN_FEE_PRICEID = process.env.BASIC_PLAN_FEE_PRICEID!;
+const BASIC_PLAN_SUB_PRICEID = process.env.BASIC_PLAN_SUB_PRICEID!;
+const PRO_PLAN_FEE_PRICEID = process.env.PRO_PLAN_FEE_PRICEID!;
+const PRO_PLAN_SUB_PRICEID = process.env.PRO_PLAN_SUB_PRICEID!;
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "");
+const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export async function POST(req: NextRequest) {
+  if (!endpointSecret) {
+    return NextResponse.json(
+      { error: "Webhook secret not configured" },
+      { status: 500 }
+    );
+  }
+
+  let event: Stripe.Event;
+
+  try {
+    // Get the raw body
+    const rawBody = await req.text();
+    const signature = req.headers.get("stripe-signature") || "";
+
+    // Validate and parse the event
+    event = stripe.webhooks.constructEvent(rawBody, signature, endpointSecret);
+  } catch (err: any) {
+    return NextResponse.json(
+      { error: `Webhook Error: ${err.message}` },
+      { status: 400 }
+    );
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case "invoice.payment_succeeded": {
+      const invoice = event.data.object;
+
+      const company = await Company.findByPk(
+        invoice.subscription_details?.metadata?.id
+      );
+      if (company) {
+        const pricids = invoice.lines.data.map((el) => el.price?.id);
+        switch (invoice.subscription_details?.metadata?.subPlan) {
+          case PlansEnum.BASIC: {
+            if (pricids.find((el) => el === BASIC_PLAN_FEE_PRICEID)) {
+              company.regPaymentStatus = true;
+              await Payments.create({
+                invoice: invoice.subscription as string,
+                paymentDate: new Date() as any,
+                plan: invoice.subscription_details?.metadata?.subPlan,
+                status: invoice.status as any,
+                companyId: company.id,
+                invoicePDF:invoice.invoice_pdf as any,
+                description: `${invoice.subscription_details?.metadata?.subPlan} fee payment completed`,
+              });
+            }
+            if (pricids.find((el) => el === BASIC_PLAN_SUB_PRICEID)) {
+              await Payments.create({
+                invoice: invoice.subscription as string,
+                paymentDate: new Date() as any,
+
+                plan: invoice.subscription_details?.metadata?.subPlan,
+                status: invoice.status as any,
+                companyId: company.id,
+                invoicePDF:invoice.invoice_pdf as any,
+
+                description: `${invoice.subscription_details?.metadata?.subPlan} subscription payment completed`,
+              });
+              company.subsriptionPaymentStatus = true;
+            }
+            company.paymentLink = null as any;
+            await company.save();
+            break;
+          }
+          case PlansEnum.PRO: {
+            if (pricids.find((el) => el === PRO_PLAN_FEE_PRICEID)) {
+              company.regPaymentStatus = true;
+              await Payments.create({
+                invoice: invoice.subscription as string,
+                paymentDate: new Date() as any,
+
+                plan: invoice.subscription_details?.metadata?.subPlan,
+                status: invoice.status as any,
+                companyId: company.id,
+                invoicePDF:invoice.invoice_pdf as any,
+
+                description: `${invoice.subscription_details?.metadata?.subPlan} fee payment completed`,
+              });
+            }
+            if (pricids.find((el) => el === PRO_PLAN_SUB_PRICEID)) {
+              await Payments.create({
+                invoice: invoice.subscription as string,
+                paymentDate: new Date() as any,
+
+                plan: invoice.subscription_details?.metadata?.subPlan,
+                status: invoice.status as any,
+                companyId: company.id,
+                invoicePDF:invoice.invoice_pdf as any,
+
+                description: `${invoice.subscription_details?.metadata?.subPlan} subscription payment completed`,
+              });
+              company.subsriptionPaymentStatus = true;
+            }
+            company.paymentLink = null as any;
+            await company.save();
+
+            break;
+          }
+        }
+      }
+
+      break;
+    }
+    case "invoice.payment_failed":
+    case "customer.subscription.deleted": {
+      const invoice: any = event.data.object; // Stripe invoice object
+
+      const company = await Company.findByPk(
+        invoice.subscription_details?.metadata?.id
+      );
+      if (company) {
+        const pricids = invoice.lines.data.map((el) => el.price?.id);
+        switch (invoice.subscription_details?.metadata?.subPlan) {
+          case PlansEnum.BASIC: {
+            if (pricids.find((el) => el === BASIC_PLAN_FEE_PRICEID)) {
+              company.regPaymentStatus = false;
+              await Payments.create({
+                invoice: invoice.subscription as string,
+
+                plan: invoice.subscription_details?.metadata?.subPlan,
+                status: "failed",
+                companyId: company.id,
+                description: `${invoice.subscription_details?.metadata?.subPlan} fee payment failed`,
+              });
+            }
+            if (pricids.find((el) => el === BASIC_PLAN_SUB_PRICEID)) {
+              await Payments.create({
+                invoice: invoice.subscription as string,
+
+                plan: invoice.subscription_details?.metadata?.subPlan,
+                status: "failed",
+
+                companyId: company.id,
+                description: `${invoice.subscription_details?.metadata?.subPlan} subscription payment failed`,
+              });
+              company.subsriptionPaymentStatus = false;
+              await company.save();
+            }
+            break;
+          }
+          case PlansEnum.PRO: {
+            if (pricids.find((el) => el === PRO_PLAN_FEE_PRICEID)) {
+              company.regPaymentStatus = false;
+              await Payments.create({
+                invoice: invoice.subscription as string,
+
+                plan: invoice.subscription_details?.metadata?.subPlan,
+                status: "failed",
+
+                companyId: company.id,
+                description: `${invoice.subscription_details?.metadata?.subPlan} fee payment failed`,
+              });
+            }
+            if (pricids.find((el) => el === PRO_PLAN_SUB_PRICEID)) {
+              await Payments.create({
+                invoice: invoice.subscription as string,
+
+                plan: invoice.subscription_details?.metadata?.subPlan,
+                status: invoice.status as any,
+                companyId: company.id,
+                description: `${invoice.subscription_details?.metadata?.subPlan} subscription payment failed`,
+              });
+              company.subsriptionPaymentStatus = false;
+              await company.save();
+            }
+            break;
+          }
+        }
+      }
+
+      break;
+    }
+    default:
+      console.log(`Unhandled event type: ${event.type}`);
+  }
+
+  // Respond to Stripe to acknowledge receipt of the event
+  return NextResponse.json({ received: true });
 }
-  
---
+++ src/app/payment-success/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <section className='container'>
+            sccess <button>Continue</button>
+        </section>
+    );
+}
+
+export default page

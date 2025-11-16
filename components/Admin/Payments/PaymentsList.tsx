a88e51e3e22da776693501872c94083b16e7862c
christin <christink388@gmail.com>
2025-11-16 23:03:16 +0530

    create payment and update payment completed

--- components/Admin/Payments/PaymentsList.tsx
+++ components/Admin/Payments/PaymentsList.tsx
@@ -1,32 +1,45 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import AgGridTable from "../AgGridTable";
 import styles from "./PaymentsList.module.scss";
 import axios from "axios";
 import { ColDef } from "ag-grid-community";
-import { PlusCircleOutlined } from "@ant-design/icons";
+import { MoreOutlined, PlusCircleOutlined } from "@ant-design/icons";
 import { SquarePlus } from "lucide-react";
 import { paymentType } from "../../PaymentSuccess/PaymentTemp";
+import { formatDate } from "@/helpers/helper";
+import CreatePayments from "./CreatePayments";
+import { Dropdown, notification } from "antd";
+import { NotificationPlacement } from "antd/es/notification/interface";
+
+type NotificationType = "success" | "info" | "warning" | "error";
+
+type NotificationMessage = {
+  type: NotificationType;
+  message: string;
+  placement: NotificationPlacement;
+};
 
 const PaymentsList = () => {
   const [rowData, setRowData] = useState<any>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const [type, setType] = useState<any>(paymentType.oneTime);
+  const [open, setOpen] = useState<boolean>(false);
+  const [api, contextHolder] = notification.useNotification();
+  const [updatePayment, setupdatePayment] = useState<any>(null);
+  const allPaymentRef = useRef<any>([]);
   useEffect(() => {
     (async () => {
       await fetchPayments(type);
     })();
   }, [type]);
 
-  function formatDate(inputDate: string): string {
-    const date = new Date(inputDate);
-
-    const day = date.getDate();
-    const month = date.toLocaleString("en-US", { month: "short" }); // e.g., "Mar"
-    const year = date.getFullYear();
-
-    return `${day} ${month} ${year}`;
-  }
+  const openNotification = (data: NotificationMessage) => {
+    api[data.type]({
+      message: data.message,
+      placement: data?.placement,
+    });
+  };
 
   const fetchPayments = async (type: string) => {
     setLoading(true);
@@ -43,7 +56,10 @@ const PaymentsList = () => {
           cName: `${el?.companyName ?? ""} ${el?.ctype ?? "--"}`,
           pFor: el?.registrationState ?? "--",
           status: el?.status,
+          createdStatus: el?.createdBy == 1 ? "System" : "Manuel",
+          createdBy: el?.createdBy,
         }));
+        allPaymentRef.current = res?.data;
         setRowData(filterData);
         setLoading(false);
       })
@@ -92,6 +108,12 @@ const PaymentsList = () => {
       filter: true,
     },
     {
+      headerName: "Created By",
+      field: "createdStatus",
+      sortable: true,
+      filter: true,
+    },
+    {
       headerName: "Status",
       field: "status",
       sortable: true,
@@ -105,89 +127,38 @@ const PaymentsList = () => {
         );
       },
     },
-    // {
-    //   headerName: "Action",
-    //   field: "action",
-    //   floatingFilter: false,
-    //   cellRenderer: (params) => {
-    //     const data = params?.data;
-    //     let menu: any = [];
-    //     if (data.status === "Active") {
-    //       menu = [
-    //         {
-    //           key: `${data?.id}-2`,
-    //           label: "Delete",
-    //           onClick: (event: any) => {
-    //             // Prevent row click
-    //             handleMenuClick(event);
-    //           },
-    //         },
-    //         {
-    //           key: `${data?.id}-0`,
-    //           label: "In Active",
-    //           onClick: (event: any) => {
-    //             // Prevent row click
-    //             handleMenuClick(event);
-    //           },
-    //         },
-    //       ];
-    //     } else if (data.status === "Deleted") {
-    //       menu = [
-    //         {
-    //           key: `${data?.id}-1`,
-    //           label: "Activate",
-    //           onClick: (event: any) => {
-    //             // Prevent row click
-    //             handleMenuClick(event);
-    //           },
-    //         },
-    //       ];
-    //     } else {
-    //       menu = [
-    //         {
-    //           key: `${data?.id}-1`,
-    //           label: "Activate",
-    //           onClick: (event: any) => {
-    //             // Prevent row click
-    //             handleMenuClick(event);
-    //           },
-    //         },
-    //         {
-    //           key: `${data?.id}-0`,
-    //           label: "In Active",
-    //           onClick: (event: any) => {
-    //             // Prevent row click
-    //             handleMenuClick(event);
-    //           },
-    //         },
-    //       ];
-    //     }
-
-    //     if (data.status === "Deleted") {
-    //       menu = [
-    //         {
-    //           key: `${data?.id}-1`,
-    //           label: "Activate",
-    //           onClick: (event: any) => {
-    //             // Prevent row click
-    //             handleMenuClick(event);
-    //           },
-    //         },
-    //       ];
-    //     }
-
-    //     return (
-    //       <Dropdown
-    //         menu={{
-    //           items: menu,
-    //         }}
-    //         trigger={["click"]}
-    //       >
-    //         <MoreOutlined style={{ fontSize: "20px" }} />
-    //       </Dropdown>
-    //     );
-    //   },
-    // },
+    {
+      headerName: "Action",
+      field: "action",
+      floatingFilter: false,
+      cellRenderer: (params) => {
+        const data = params?.data;
+        let menu: any = [];
+        if (data?.createdBy === 1) {
+          return "";
+        }
+        menu = [
+          {
+            key: `${data?.id}-1`,
+            label: "Edit",
+            onClick: (event: any) => {
+              // Prevent row click
+              handleMenuClick(event);
+            },
+          },
+        ];
+        return (
+          <Dropdown
+            menu={{
+              items: menu,
+            }}
+            trigger={["click"]}
+          >
+            <MoreOutlined style={{ fontSize: "20px" }} />
+          </Dropdown>
+        );
+      },
+    },
   ]);
 
   const handleRowClick = (params: any) => {
@@ -202,9 +173,37 @@ const PaymentsList = () => {
     }
   };
 
+  const handleCreatePayment = (ptype: any) => {
+    if (ptype === type) {
+      fetchPayments(ptype);
+      setOpen(false);
+      setupdatePayment(null);
+    } else {
+      setOpen(false);
+      setType(ptype);
+      setupdatePayment(null);
+    }
+  };
+
+  const handleMenuClick = (e: any) => {
+    const allPayment = allPaymentRef.current;
+    if (!allPayment?.length) {
+      console.warn("allCompanies is undefined at click time!");
+      return;
+    }
+    const companyId = e?.key?.split("-")[0];
+    const splitedNumber = Number(companyId);
+    const findCompanyDetails = allPayment?.find(
+      (el: any) => el?.id === splitedNumber
+    );
+    setupdatePayment(findCompanyDetails || null);
+    setOpen(true);
+  };
+
   return (
     <>
       <div className={styles.headerWrapper}>
+        {contextHolder}
         <div className={styles.leftSide}>
           <button
             type="button"
@@ -222,20 +221,21 @@ const PaymentsList = () => {
           </button>
         </div>
         <div className={styles.rightSide}>
-          {/* <button
-            type="button"
-            onClick={() => exportCompanies(companyStatus)}
-            disabled={loading}
-          >
-            <ArrowDownToLine fontSize={16} />
-            Export
-          </button> */}
-          <button type="button">
+          <button type="button" onClick={() => setOpen(true)}>
             <SquarePlus />
             Create New
           </button>
         </div>
       </div>
+
+      <CreatePayments
+        onClose={() => setOpen(false)}
+        open={open}
+        onSuccess={handleCreatePayment}
+        openNotification={openNotification}
+        updatePayment={updatePayment}
+      />
+
       <div className={styles.agridWrapper}>
         <AgGridTable
           columnDefs={columnDefs}
